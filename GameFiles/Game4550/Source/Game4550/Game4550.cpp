b3aac8f0df3e4e178bdcc7b9bdd645196c082974
Luna <153253648+Bro-dep@users.noreply.github.com>
2026-09-11 12:49:52 -0700

    init

--- GameFiles/Game4550/Source/Game4550/Game4550.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Game4550.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Game4550, "Game4550" );
-
-DEFINE_LOG_CATEGORY(LogGame4550)

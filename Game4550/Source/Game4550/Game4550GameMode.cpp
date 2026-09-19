1448e1da6687676c26b3273be2ec45e116232395
Luna <153253648+Bro-dep@users.noreply.github.com>
2026-09-19 14:39:46 -0700

    delete everything

--- Game4550/Source/Game4550/Game4550GameMode.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Game4550GameMode.h"
-#include "Game4550PlayerController.h"
-
-AGame4550GameMode::AGame4550GameMode()
-{
-	PlayerControllerClass = AGame4550PlayerController::StaticClass();
-}
